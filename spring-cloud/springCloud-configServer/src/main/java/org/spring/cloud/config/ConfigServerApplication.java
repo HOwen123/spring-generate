f9d215a1e4da8f889b08bb42166e444987948a07
chenssy89 <chenssy995812509@163.com>
2017-06-18 11:23:06 +0800

    add config server

---
+++ spring-cloud/springCloud-configServer/src/main/java/org/spring/cloud/config/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package org.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author chenssy
+ * @date 2017/6/18
+ * @since v1.0.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
