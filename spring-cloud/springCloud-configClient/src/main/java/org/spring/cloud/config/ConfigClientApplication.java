300985535d0276b45d6a909c5224496e5947576c
chenssy89 <chenssy995812509@163.com>
2017-06-18 22:27:40 +0800

    配置中心 服务化

--- spring-cloud/springCloud-configClient/src/main/java/org/spring/cloud/config/ConfigClientApplication.java
+++ spring-cloud/springCloud-configClient/src/main/java/org/spring/cloud/config/ConfigClientApplication.java
@@ -2,6 +2,7 @@ package org.spring.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author chenssy
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since v1.0.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigClientApplication {
     public static void main(String[] args){
         SpringApplication.run(ConfigClientApplication.class,args);
